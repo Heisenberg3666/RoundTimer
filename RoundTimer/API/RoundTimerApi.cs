12708337924d29f47a738de3661dbd56bb7498b3
Dylan Morris <dylanmorris330@gmail.com>
2022-09-13 21:21:13 +0100

    Initial

---
+++ RoundTimer/API/RoundTimerApi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using AdvancedHints.Components;
+using Exiled.API.Features;
+using MEC;
+
+namespace RoundTimer.API
+{
+    public static class RoundTimerApi
+    {
+        private static TimeSpan _oldRoundTime = Round.ElapsedTime;
+        internal static CoroutineHandle Coroutine;
+
+        public static bool RoundTimeChanged
+        {
+            get
+            {
+                bool roundTimeChanged = _oldRoundTime != Round.ElapsedTime;
+                _oldRoundTime = Round.ElapsedTime;
+
+                return roundTimeChanged;
+            }
+        }
+
+        public static IEnumerator<float> DisplayRoundTime()
+        {
+            while (true)
+            {
+                if (!RoundTimeChanged) continue;
+                
+                foreach (Player player in Player.List)
+                {
+                    HudManager.ShowHint(player, $"Round Time: {FormatTime(Round.ElapsedTime)}", 1f);
+                }
+
+                yield return Timing.WaitForOneFrame;
+            }
+        }
+
+        private static string FormatTime(TimeSpan time) => 
+            time.Minutes < 1 ? $"{time.Seconds}s" : $"{time.Minutes}m {time.Seconds}s";
+    }
+}
---
+++ RoundTimer/Events/ServerEvents.cs
@@ -0,0 +1,32 @@
+﻿using Exiled.Events.EventArgs;
+using Exiled.Events.Handlers;
+using MEC;
+using RoundTimer.API;
+
+namespace RoundTimer.Events
+{
+    internal class ServerEvents
+    {
+        public ServerEvents()
+        {
+            Server.RoundStarted += OnRoundStarted;
+            Server.RoundEnded += OnRoundEnded;
+        }
+
+        ~ServerEvents()
+        {
+            Server.RoundStarted -= OnRoundStarted;
+            Server.RoundEnded -= OnRoundEnded;
+        }
+
+        private void OnRoundStarted()
+        {
+            RoundTimerApi.Coroutine = Timing.RunCoroutine(RoundTimerApi.DisplayRoundTime());
+        }
+
+        private void OnRoundEnded(RoundEndedEventArgs e)
+        {
+            Timing.KillCoroutines(RoundTimerApi.Coroutine);
+        }
+    }
+}
