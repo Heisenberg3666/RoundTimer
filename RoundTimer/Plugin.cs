5477821bd59aa4e3b9e97d7fc2eff96891c13350
Dylan Morris <dylanmorris330@gmail.com>
2022-09-13 21:24:01 +0100

    Project stuff

---
+++ RoundTimer/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Exiled.API.Features;
+using RoundTimer.Events;
+
+namespace RoundTimer
+{
+    public class Plugin : Plugin<Config>
+    {
+        private ServerEvents _serverEvents;
+        
+        public override string Name { get; } = nameof(RoundTimer);
+        public override string Author { get; } = "Heisenberg3666";
+        public override Version Version { get; } = new Version(1, 0, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(5, 3, 0);
+
+        public override void OnEnabled()
+        {
+            _serverEvents = new ServerEvents();
+            
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            _serverEvents = null;
+            
+            base.OnDisabled();
+        }
+    }
+}
